a61e4354559e097f858f857ba9ad02e1b0c41c30
jenbuck <jrbuck31@gmail.com>
2019-11-17 13:30:23 -0600

    completed toSting and Equals methods. Still need to fix GPA updates. Credits update correctly

--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -1,5 +1,7 @@
 package org.launchcode.java.demos.lsn4classes2;
 
+import java.util.Objects;
+
 public class Student {
 
     private static int nextStudentId = 1;
@@ -30,20 +32,50 @@ public class Student {
 
 
      //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+    public String getGradeLevel() {// Determine the grade level of the student based on numberOfCredits
+        if(this.numberOfCredits >= 0 && this.numberOfCredits <=29){
+            return (this.name + " is a Freshman");
+        }else if(this.numberOfCredits >=30 && this.numberOfCredits <=59){
+            return (this.name + " is a Sophomore");
+        }else if(this.numberOfCredits >=60 && this.numberOfCredits <=89){
+            return (this.name + " is a Junior");
+        }else{
+            return (this.name + " is a Senior");
+        }
+    }
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
         // Update the appropriate fields: numberOfCredits, gpa
+        //gpa = (total quality score) / (total number of credits)
+        double currentTotalQualityScore = this.gpa/this.numberOfCredits;
+        int updatedCourseCredits = this.numberOfCredits + courseCredits;
+        double updatedTotalQualityScore = (this.gpa + grade)*(updatedCourseCredits);
+        this.gpa = updatedTotalQualityScore/updatedCourseCredits;//THIS NEEDS TO BE FIXED
+        this.numberOfCredits = updatedCourseCredits;
     }
 
-    // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
+    // custom 'toString' method here. Make sure it returns a well-formatted String rather
     //  than just the class fields.
+    public String toString() {
+        return name + " (StudentID: " +studentId + ", Credits: " + numberOfCredits + ", GPA: " + gpa + ")";
+    }
 
-    // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
+    // custom 'equals' method here. Consider which fields should match in order to call two
     //  Student objects equal.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId == student.studentId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId);
+    }
 
     public String getName() {
         return name;
@@ -81,9 +113,12 @@ public class Student {
         Student sally = new Student("Sally",1,1,4.0);
         System.out.println("The Student class works! " + sally.getName() + " is a student!");
         System.out.println(sally);
+        System.out.println(sally.getGradeLevel());
         sally.addGrade(12, 3.5);
         System.out.println(sally);
+        System.out.println(sally.getGradeLevel());
         sally.addGrade(25, 3.8);
         System.out.println(sally);
+        System.out.println(sally.getGradeLevel());
     }
 }
