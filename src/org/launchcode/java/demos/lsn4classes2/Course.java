941ecd4afac6e2359b699e854a09159ecee96f4c
jenbuck <jrbuck31@gmail.com>
2019-11-17 18:27:34 -0600

    completed toSting and Equals methods for Course class. Still need to fix GPA updates.

--- src/org/launchcode/java/demos/lsn4classes2/Course.java
+++ src/org/launchcode/java/demos/lsn4classes2/Course.java
@@ -1,17 +1,46 @@
 package org.launchcode.java.demos.lsn4classes2;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Course {
     private String topic;
     private Teacher instructor;
     private ArrayList<Student> enrolledStudents;
+    private String courseName;
+    private String courseIDNumber;
+
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather than
     //  just the class fields.
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "topic='" + topic + '\'' +
+                ", instructor=" + instructor +
+                ", enrolledStudents=" + enrolledStudents +
+                ", courseName='" + courseName + '\'' +
+                ", courseIDNumber='" + courseIDNumber + '\'' +
+                '}';
+    }
 
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Course objects equal.
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(courseName, course.courseName) &&
+                Objects.equals(courseIDNumber, course.courseIDNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(courseName, courseIDNumber);
+    }
 }
